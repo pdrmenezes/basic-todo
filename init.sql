851b4bc6bd3cd9e37bc3b71f51d5f85d020517f8
Pedro Menezes <pdrmenezes1@gmail.com>
2025-09-25 23:08:53 -0300

    chore: remove test file

--- init.sql
+++
@@ -1,8 +0,0 @@
--- Initialize the basic_todo database
--- This file is automatically executed when the PostgreSQL container starts
-
--- Create extensions if needed
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
--- The tables will be created by Drizzle migrations
--- This file is here for any additional setup if needed
